edc8731cc8cd4cf931990f3088a62e0f61f4e45f
owourviny109 <owourvincent069@gmail.com>
2025-04-12 09:45:34 +0300

    CHOME

--- Pages/Company/CHome.php
+++ Pages/Company/CHome.php
@@ -219,7 +219,7 @@ try {
             </div>
         </div>
     </div>
-    <!--
+    
      
     <footer class="bg-dark text-white text-center py-3 mt-auto">
         <p class="mb-0">&copy; 2025 AttachME. All rights reserved.</p>
@@ -229,7 +229,7 @@ try {
             <a href="../../contact.php" class="text-white fw-bold">Contact Support: attachme@admin</a>
         </div>
     </footer>
-    -->
+    
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
